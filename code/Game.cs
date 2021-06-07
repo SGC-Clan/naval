d3fc46622b8ba31da1ad5d5c7c81aea8f508da29
José Guedes (AltairPT) <altair@sgc-clan.com>
2021-06-07 13:28:00 +0100

    Initial Upload

---
+++ code/Game.cs
@@ -0,0 +1,97 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+[Library( "naval", Title = "Naval" )]
+partial class NavalGame : Game
+{
+	public NavalGame()
+	{
+		if ( IsServer )
+		{
+			// Create the HUD
+			new SandboxHud();
+		}
+	}
+
+	public override void ClientJoined( Client cl )
+	{
+		base.ClientJoined( cl );
+		var player = new NavalPlayer();
+		player.Respawn();
+
+		cl.Pawn = player;
+	}
+
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+	}
+
+	[ServerCmd( "spawn" )]
+	public static void Spawn( string modelname )
+	{
+		var owner = ConsoleSystem.Caller?.Pawn;
+
+		if ( ConsoleSystem.Caller == null )
+			return;
+
+		var tr = Trace.Ray( owner.EyePos, owner.EyePos + owner.EyeRot.Forward * 500 )
+			.UseHitboxes()
+			.Ignore( owner )
+			.Size( 2 )
+			.Run();
+
+		var ent = new Prop();
+		ent.Position = tr.EndPos;
+		ent.Rotation = Rotation.From( new Angles( 0, owner.EyeRot.Angles().yaw, 0 ) ) * Rotation.FromAxis( Vector3.Up, 180 );
+		ent.SetModel( modelname );
+
+		// Drop to floor
+		if ( ent.PhysicsBody != null && ent.PhysicsGroup.BodyCount == 1 )
+		{
+			var p = ent.PhysicsBody.FindClosestPoint( tr.EndPos );
+
+			var delta = p - tr.EndPos;
+			ent.PhysicsBody.Position -= delta;
+			//DebugOverlay.Line( p, tr.EndPos, 10, false );
+		}
+
+	}
+
+	[ServerCmd( "spawn_entity" )]
+	public static void SpawnEntity( string entName )
+	{
+		var owner = ConsoleSystem.Caller.Pawn;
+
+		if ( owner == null )
+			return;
+
+		var attribute = Library.GetAttribute( entName );
+
+		if ( attribute == null || !attribute.Spawnable )
+			return;
+
+		var tr = Trace.Ray( owner.EyePos, owner.EyePos + owner.EyeRot.Forward * 200 )
+			.UseHitboxes()
+			.Ignore( owner )
+			.Size( 2 )
+			.Run();
+
+		var ent = Library.Create<Entity>( entName );
+		if ( ent is BaseCarriable && owner.Inventory != null )
+		{
+			if ( owner.Inventory.Add( ent, true ) )
+				return;
+		}
+
+		ent.Position = tr.EndPos;
+		ent.Rotation = Rotation.From( new Angles( 0, owner.EyeRot.Angles().yaw, 0 ) );
+
+		//Log.Info( $"ent: {ent}" );
+	}
+}
