c2f64e134f4a8bde172020bb1f135e618a5b76e3
sernikb <sernikb@gmail.com>
2021-07-01 15:18:51 +0200

    Fixed PhysGun viewmodel once again..

--- code/tools/PhysGun.cs
+++ code/tools/PhysGun.cs
@@ -6,7 +6,7 @@ using System.Linq;
 [Library( "physgun" )]
 public partial class PhysGun : Carriable
 {
-	public override string ViewModelPath => "weapons/rust_pistol/v_rust_pistol.vmdl";
+	public override string ViewModelPath => "models/gmod/weapons/v_superphyscannon.vmdl";
 
 	protected PhysicsBody holdBody;
 	protected WeldJoint holdJoint;
@@ -39,7 +39,7 @@ public partial class PhysGun : Carriable
 	{
 		base.Spawn();
 
-		SetModel( "models/gmod/weapons/v_superphyscannon.vmdl" );
+		SetModel( "weapons/rust_pistol/rust_pistol.vmdl" );
 
 		CollisionGroup = CollisionGroup.Weapon;
 		SetInteractsAs( CollisionLayer.Debris );
