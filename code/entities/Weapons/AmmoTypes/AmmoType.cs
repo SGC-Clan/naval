70b26a7a09bf559de3d8285d638b272806ad0a33
sernikb <sernikb@gmail.com>
2023-08-09 03:28:54 +0200

    turret system using bone rotation

---
+++ code/entities/Weapons/AmmoTypes/AmmoType.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+	//Truth is - its more like turrets stats than only ammo, but I named it like that in early 2017 and I dont wanna fix it okay?
+	public struct AmmoType
+	{
+		//===|| Turret specific stats:
+
+		public string NiceName;
+		
+		public string Description;	
+
+		public string Tags;
+
+		public string Author;
+
+		public string LogoImage;
+
+		public int Price;
+
+		public string TurretModel;
+
+		//basic damage
+		public float Damage;
+
+		//randomise base damage
+		public float DamageRandomnes;
+
+		public float DamageMultVsShips;
+		public float DamageMultVsSubmarines;
+		public float DamageMultVsPlanes;
+		public float DamageMultVsAirships;
+
+		//force applied to the gun when firing
+		public float Recoil;
+
+		public float Inaccuracy;
+
+		//How much time it takes to reload
+		public float ReloadTime;
+
+		//how much times we can shoot (with firerate as delay) without reloading
+		public int ClipSize;
+
+		//how much time between the shoots from the clip
+		public float Firerate;
+
+		public float LaunchSpeed;
+
+		public float MaxLifeTime;
+
+		public string Decal;
+
+		//  ======TO:DO======
+
+		//after this distance projectile will trajectory will start to move around due to randomly decreased speed and external forces
+		//public float effectiveRange;
+
+		//public float Drag;
+
+		//public float Gravity;
+
+		//public float Mass;
+
+		//public float PenetrationCoefficient;
+
+		//public float DeflectionCoefficient;
+
+		// Standard = using fake physics with penetration systems
+		// Physics = yeeting source2 physics objects at great distances
+		// Hitscan = like laser with limited ranges
+		// Flame = simple sticky fluid simulation
+		// Spawner = spawns another selected entity
+		//public string simulationTechnique;
+	}
+}
--- code/entities/Weapons/BaseTurret.cs
+++ code/entities/Weapons/BaseTurret.cs
@@ -37,17 +37,10 @@ namespace Sandbox.entities.Weapons
 			//new Vector3( 61.50f, 0, 64.93f )
 
 			new Vector3( 0, 0, 0 ),
-			new Vector3( 0, 0, 2.3f ),
-			new Vector3( -3.26f, 0, 16.07f ),
-			new Vector3( 41.74f, 0, 18.07f ),
-		};
-
-		public Angles[] BoneAngles = new Angles[]
-		{
-			new Angles( 0, 0, 0 ),
-			new Angles( 0, 90, 0 ),
-			new Angles( 0, 0, 0 ),
-			new Angles( 0, 0, 0 ),
+			new Vector3( 0, 0, 2.35f ),
+			new Vector3( -3.225f, 0, 13.75f ),
+			new Vector3( 46f, 0, 2f ),
+			new Vector3( 2.5f, 0, 0 ),
 		};
 
 		public override void Spawn()
@@ -57,19 +50,21 @@ namespace Sandbox.entities.Weapons
 			SetModel( "models_and_materials/weapons/m1917/m1917.vmdl" ); //"models_and_materials/weapons/45mm_gun/45mm_gun.vmdl"
 		}
 
-		public override void OnNewModel( Model model ) 
+		public override void OnNewModel( Model model )
 		{
 			base.OnNewModel( model );
 
 			//collect all the original spooky bonez 
 			OgBones = Model.Bones;
 
+
+			Log.Info( "---- Bones for Model: "+Model.ResourceName+" ----" );
 			for ( int i = 0; i < model.BoneCount; i++ )
 			{
 				int boneParent = model.GetBoneParent( i );
 				if ( i >= 0 )
 				{
-					Log.Info( "Bone Name: "+ model.GetBoneName( i ) );
+					Log.Info( "Bone Name: " + model.GetBoneName( i ) );
 					Log.Info( "Pos: " + model.GetBoneTransform( i ).Position );
 					Log.Info( "Ang: " + model.GetBoneTransform( i ).Rotation.Angles() );
 				}
@@ -80,8 +75,8 @@ namespace Sandbox.entities.Weapons
 		public void Tick() 
 		{
 			//if player is too far away, reset
-			//if ( User != null && User.Position.Distance( Position ) > 100 )
-			//	User = null;
+			if ( User != null && User.Position.Distance( Position ) > 100 )
+				User = null;
 
 			CurrentAimAngle = new Angles( 0,0,90 );
 
@@ -95,82 +90,28 @@ namespace Sandbox.entities.Weapons
 		[GameEvent.Client.Frame]
 		public void OnFrame()
 		{
-			if ( OgBones == null )
-				return;
 
-			//some stuff I probably need:
-			//SetBoneTransform - Set real physical bone transform
-			//SetBone - Set visual only bone transform
-			//Rotation.SmoothDamp();
-			//ResetBones();
-
-			// Drone Example:
-			// var transform = Transform.ToWorld( new Transform( turbinePositions[i] * Scale, Rotation.From( new Angles( 0, spinAngle, 0 ) ) ) );
-			// transform.Scale = Scale;
-			// SetBoneTransform( i, transform );
-
-
-
-			// ============== Working test ==============
-			var TestBonePos1 = new Vector3( 0, 0, 2.35f );
-			var TestBoneAng1 = new Angles( 0, 0, 0 );
-
-			var NewYawTransform = Transform.ToWorld( new Transform( TestBonePos1 * Scale, Rotation.From( TestBoneAng1 + new Angles( 0, CurrentAimAngle.yaw, 0 ) ) ) );
+			// ============== Turret aiming animation ==============
+			var BonePos1 = BonePositions[1];
+			var NewYawTransform = Transform.ToWorld( new Transform( BonePos1 * Scale, Rotation.From( new Angles( 0, CurrentAimAngle.yaw, 0 ) ) ) );
 			NewYawTransform.Scale = Scale;
 			SetBoneTransform( "yaw", NewYawTransform );
 
-			var TestBonePos2 = new Vector3( -3.225f, 0, 13.75f );
-			var TestBoneAng2 = new Angles( 0, 0, 0 );
-
-			var NewPitchTransform = NewYawTransform.ToWorld( new Transform( TestBonePos1 + TestBonePos2 * Scale, Rotation.From( TestBoneAng2 + new Angles( CurrentAimAngle.pitch, 0, 0 ) ) ) );
+			var BonePos2 = BonePositions[2];
+			var NewPitchTransform = NewYawTransform.ToWorld( new Transform( BonePos1 + BonePos2 * Scale, Rotation.From( new Angles( CurrentAimAngle.pitch, 0, 0 ) ) ) );
 			NewPitchTransform.Scale = Scale;
-			//NewPitchTransform.Position = NewPitchTransform.PointToWorld( TestBonePos2 * -1 );
-			NewPitchTransform = NewPitchTransform.RotateAround( Transform.Position, Rotation.From( TestBoneAng2 ) );
 			SetBoneTransform( "pitch", NewPitchTransform );
 
-			var TestBonePos3 = new Vector3( 46f, 0, 2f );
-			var TestBoneAng3 = new Angles( 0, 0, 0 );
-
-			var NewBarrelTransform = NewPitchTransform.ToWorld( new Transform( TestBonePos3 * Scale, Rotation.From( TestBoneAng3 ) ) );
+			var BonePos3 = BonePositions[3];
+			var NewBarrelTransform = NewPitchTransform.ToWorld( new Transform( BonePos3 * Scale, Rotation.From( new Angles( 0, 0, 0 ) ) ) );
 			NewBarrelTransform.Scale = Scale;
-			//NewBarrelTransform = NewBarrelTransform.RotateAround( Transform.Position, Rotation.From( TestBoneAng3 ) );
 			SetBoneTransform( "barrel01", NewBarrelTransform );
 
-			var TestBonePos4 = new Vector3( 2.5f, 0, 0 );
-			var TestBoneAng4 = new Angles( 0, 0, 0 );
-
-			var NewBarrelEndTransform = NewBarrelTransform.ToWorld( new Transform( TestBonePos4 * Scale, Rotation.From( TestBoneAng4 ) ) );
+			var BonePos4 = BonePositions[4];
+			var NewBarrelEndTransform = NewBarrelTransform.ToWorld( new Transform( BonePos4 * Scale, Rotation.From( new Angles( 0, 0, 0 ) ) ) );
 			NewBarrelEndTransform.Scale = Scale;
 			SetBoneTransform( "barrel01_end", NewBarrelEndTransform );
 			// =============================================
-
-
-			//Local bone transform test
-
-			//var yawBoneTransform = OgBones.GetBone( "yaw" ).LocalTransform;
-			//var yawRotationOffset = Rotation.From( 0, 0, 0 );
-			//var yawTransform = Transform.ToWorld( new Transform( yawBoneTransform.Position * Scale, Rotation.From( new Angles( 0, CurrentAimAngle.yaw, 0 ) ) * yawRotationOffset ) );
-			//yawTransform.Scale = Scale;
-			//SetBoneTransform( "yaw", yawTransform );
-
-			//var pitchBoneTransform = OgBones.GetBone( "pitch" ).LocalTransform;
-			//var pitchRotationOffset = Rotation.From(0,0,0);
-			//var pitchTransform = Transform.ToWorld( new Transform( pitchBoneTransform.Position * Scale, Rotation.From( new Angles( CurrentAimAngle.pitch, CurrentAimAngle.yaw, 0 ) ) * pitchRotationOffset ) );
-			//pitchTransform.Scale = Scale;
-			//SetBoneTransform( "pitch", pitchTransform );
-
-
-			//var OgYawBone = OgBones.GetBone( "yaw" );
-			//var YawAngleOffset = new Angles( 0, 0, 0 ); //Angles( 33, 0, 90 )
-			//Rotation CurrentYawTarget = Transform.Rotation * Rotation.From( new Angles( 0, CurrentAimAngles.yaw + 180, 0 ) + YawAngleOffset );
-			//Transform NewYawTransform = new Transform( Transform.ToWorld( OgYawBone.LocalTransform ).Position, CurrentYawTarget ); //Transform.Position + OgYawBone.LocalTransform.Position
-			//SetBoneTransform( "yaw", NewYawTransform );
-
-			//var OgPitchBone = OgBones.GetBone( "pitch" );
-			//var PitchAngleOffset = new Angles( 0, 0, 0 );
-			//Rotation CurrentPitchTarget = Transform.Rotation * Rotation.From( new Angles( CurrentAimAngles.pitch, CurrentAimAngles.yaw, 0 ) + YawAngleOffset + PitchAngleOffset );
-			//Transform NewPitchTransform = new Transform( Transform.ToWorld( OgPitchBone.LocalTransform ).Position, CurrentPitchTarget );
-			//SetBoneTransform( "pitch", NewPitchTransform );
 
 			DebugSkeleton( this, Color.Red, 0, false );
 		}
